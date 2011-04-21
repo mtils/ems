93323663bda1306fe00ac742806208100cceaddc
Michael Tils <mtils@web-utils.de>
2011-04-21 11:26:19 +0000

    Property nach protected geändert

--- core/proxies/__init__.py
+++ core/proxies/__init__.py
@@ -5,22 +5,23 @@ Created on 21.04.2011
 @author: michi
 '''
 class AttributeProxyR(object):
+    
     def __init__(self, baseObject):
-        self.__baseObject = baseObject
+        self._baseObject = baseObject
         
     def getBaseObject(self):
-        return self.__baseObject
+        return self._baseObject
     
     def setBaseObject(self, baseObject):
-        self.__baseObject = baseObject
+        self._baseObject = baseObject
     
     def delBaseObject(self):
-        self.__baseObject = None
+        self._baseObject = None
         
     def __getattr__(self,key):
         try:
             return object.__getattribute__(self,key)
         except AttributeError:
-            return self.__baseObject.__getattribute__(key)
+            return self._baseObject.__getattribute__(key)
     
     baseObject = property(getBaseObject,setBaseObject,delBaseObject,'')
