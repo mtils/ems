f5dee53b2a74f2557e27fde3d7e310abb96a4822
Michael Tils <mtils@web-utils.de>
2011-06-07 11:29:34 +0000

    Angefangen, den Solar- und Energierechner sauber ins Suchergebnis einzubauen

---
+++ qt4/gui/itemdelegate/actionvheaderview.py
@@ -0,0 +1,69 @@
+'''
+Created on 07.06.2011
+
+@author: michi
+'''
+from PyQt4.QtCore import Qt, QSize, QRect
+from PyQt4.QtGui import QHeaderView, QAction, QTableView
+
+class ParamAction(QAction):
+    def triggerWithParams(self, model, row):
+        self.trigger()
+        
+class ActionVHeaderView(QHeaderView):
+    
+    def __init__(self, iconSize=16, parent=None):
+        if not isinstance(parent, QTableView):
+            raise TypeError("Parent has to be QTableView")
+        super(ActionVHeaderView, self).__init__(Qt.Vertical, parent)
+        self._actions = []
+        self.margin = 4
+        if isinstance(iconSize, QSize):
+            self.setIconSize(iconSize)
+        else:
+            self.setIconSize(QSize(iconSize,iconSize))
+        
+    def mousePressEvent(self, event):
+        action = self.actionAt(event.pos())
+        
+        if action is not None:
+            if isinstance(action, ParamAction):
+                action.triggerWithParams(self.parent().model(),
+                                         self.parent().rowAt(event.y()))
+            else:
+                action.trigger()
+    
+    def mouseMoveEvent(self, event):
+        action = self.actionAt(event.pos())
+        if action is not None:
+            self.setToolTip(action.text())
+    
+    def actionAt(self, point):
+        for action in self._actions:
+            for rect in action.localRects:
+                if rect.contains(point):
+                    return action
+    
+    def sizeHint(self):
+        width = (len(self._actions)* (self.iconSize().width() + self.margin)) +\
+            self.margin
+         
+        return QSize(width,24)
+    
+    def addAction(self, action):
+        action.localRects = []
+        self._actions.append(action)
+
+    def paintSection(self, painter, rect, index):
+        
+        xOffset = rect.x()
+        for action in self._actions:
+            iconRect = QRect(xOffset+self.margin,
+                             rect.y() + ((rect.height()/2) - (self.iconSize().height()/2)),
+                             self.iconSize().width(),
+                             self.iconSize().height())
+            
+            action.localRects.append(iconRect)
+            painter.drawImage(iconRect,action.icon().pixmap(self.iconSize()).toImage())
+            xOffset += (self.iconSize().width() + self.margin)
+    
