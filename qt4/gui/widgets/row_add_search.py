890838a513828d5ac671b4c2e7f99f6a0a9de757
Michael Tils <mtils@web-utils.de>
2011-06-24 11:45:07 +0000

    -Weiter an komplexer Suche gearbeitet

--- qt4/gui/widgets/row_add_search.py
+++ qt4/gui/widgets/row_add_search.py
@@ -6,7 +6,7 @@ Created on 23.06.2011
 import sys
 
 from PyQt4.QtCore import QObject, QVariant, SIGNAL, SLOT, pyqtSignal, pyqtSlot
-from PyQt4.QtGui import QWidget, QComboBox, QCheckBox, QLineEdit, QGridLayout
+from PyQt4.QtGui import QWidget, QComboBox, QCheckBox, QLineEdit, QGridLayout, QPushButton
 
 class SearchRow(QObject):
     
@@ -16,10 +16,13 @@ class SearchRow(QObject):
     def __init__(self, row, parent=None):
         super(SearchRow, self).__init__(parent)
         self._row = row
+        
         if self._row == 0:
+            self.addButton = QWidget(parent)
             self.logicalInput = QWidget(parent)
             self.logicalInput.setMinimumWidth(80)
         else:
+            self.addButton = QPushButton("+", parent)
             self.logicalInput = QComboBox(parent)
             self.connect(self.logicalInput, SIGNAL("currentIndexChanged(int)"),
                          self, SLOT("onLogicalCurrentIndexChanged(int)"))
@@ -32,6 +35,8 @@ class SearchRow(QObject):
         self._enabled = True
         if self._row != 0:
             self.setDisabled()
+        
+        
     
     @pyqtSlot(int)
     def onLogicalCurrentIndexChanged(self, index):
@@ -42,8 +47,8 @@ class SearchRow(QObject):
                 self.setEnabled()
     
     def setEnabled(self, enabled=True):
-        for widget in (self.fieldInput, self.operatorInput, self.valueInput,
-                       self.matchesInput):
+        for widget in (self.logicalInput, self.fieldInput, self.operatorInput,
+                       self.valueInput, self.matchesInput):
             widget.setEnabled(enabled)
         self._enabled = enabled
         self.rowEnabled.emit(self._enabled)
@@ -53,14 +58,27 @@ class SearchRow(QObject):
         return self.setEnabled(enabled)
          
     def setupUi(self):
+        
         if self._row != 0:
-            self.logicalInput.addItem(self.tr("Aktivieren"), QVariant("NONE"))
+            #self.logicalInput.addItem(self.tr("Aktivieren"), QVariant("NONE"))
             self.logicalInput.addItem(self.tr("Und"), QVariant("AND"))
             self.logicalInput.addItem(self.tr("Oder"), QVariant("OR"))
         
         self.fieldInput.addItem("Name")
         self.matchesInput.setChecked(True)
         self.matchesInput.setText(self.tr("Trifft zu"))
+    
+    def __del__(self):
+        print "Destructor called"
+    
+    def delete(self):
+        self.logicalInput.close()
+        self.fieldInput.close()
+        self.operatorInput.close()
+        self.valueInput.close()
+        self.matchesInput.close()
+        print "delete()"
+    
 
 class RowAddSearch(QWidget):
     def __init__(self, parent=None):
@@ -72,25 +90,53 @@ class RowAddSearch(QWidget):
         self.setLayout(QGridLayout(self))
         self.addRow()
         self.addRow()
+        
+    @pyqtSlot(bool)
+    def _onRowEnabled(self, enabled):
+        if enabled:
+            self.addRow()
+        else:
+            searchRow = self.sender()
+            i = 0
+            for row in self._rows:
+                if row is searchRow:
+                    self.removeRow(i)
+                    break
+                i += 1 
+            
     
     @pyqtSlot()
     def addRow(self):
-        print "addRow"
+        #print "addRow"
         currentRow = len(self._rows)
         if currentRow < 0:
             currentRow = 0
 
         row = SearchRow(currentRow, self)
-        layout = self.layout()
-        layout.addWidget(row.logicalInput,currentRow,0)
-        layout.addWidget(row.fieldInput,currentRow,1)
-        layout.addWidget(row.operatorInput,currentRow,2)
-        layout.addWidget(row.valueInput,currentRow,3)
-        layout.addWidget(row.matchesInput,currentRow,4)
         
+        layout = self.layout()
+        layout.addWidget(row.addButton,currentRow,0)
+        layout.addWidget(row.logicalInput,currentRow,1)
+        layout.addWidget(row.fieldInput,currentRow,2)
+        layout.addWidget(row.operatorInput,currentRow,3)
+        row.valueInput.setText("%s" % currentRow)
+        layout.addWidget(row.valueInput,currentRow,4)
+        layout.addWidget(row.matchesInput,currentRow,5)
+        
+#        self.connect(row, SIGNAL('rowEnabled(bool)'),
+#                     self, SLOT('addRow()'))
         self.connect(row, SIGNAL('rowEnabled(bool)'),
-                     self, SLOT('addRow()'))
+                     self, SLOT('_onRowEnabled(bool)'))
         self._rows.append(row)
+    
+    def removeRow(self, idx):
+        row = self._rows[idx]
+        print "removeRow %s" % idx
+        print "%s %s" % (idx, row)
+        self._rows.remove(row)
+        row.delete()
+        #del row
+        
 
 if __name__ == "__main__":
     from PyQt4.QtGui import QApplication
