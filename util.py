b003fb69ce0212e0bed819c2204b699a1a757869
Michael Tils <mtils@web-utils.de>
2011-07-12 18:11:10 +0000

    stringSingularity Funktion geschrieben um den Unterschied zwischen zwei Strings zu messen (Prozentual oder Anzahl der Löschungen/Änderungen/Hinzufügungen)

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''
 Created on 02.07.2011
 
@@ -47,3 +48,45 @@ class GenClause(object):
     
     def __str__(self):
         return "%s %s %s" % (self.left, self.operator, self.right)
+    
+def stringSimilarity(a,b, returnPercent=False):
+    "Calculates the Levenshtein distance between a and b."
+    aLen = len(a)
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a,b = b,a
+        n,m = m,n
+        
+    current = range(n+1)
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*m
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+    if returnPercent:
+        #print "Lang: %s" % aLen
+        #rightLetters = float(aLen - current[n])
+        #print "Richtige: %s" % rightLetters
+        #print "Richtig: %s" % ( aLen / rightLetters)
+        result = 100.0 - (float(current[n]) / (float(aLen)/100.0))
+        if result < 0.0:
+            return 0.0
+        return result
+    return current[n]
+
+if __name__ == '__main__':
+    tests = (
+             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstraße"),
+             ("Östliche Rheinbrückenstraße","Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Östl. Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Marienweg"),
+             ("Hauptstraße","Hauptstr."),
+             )
+    for test in tests:
+        print "%s (%s) %s:" % (test[0], len(test[0]), test[1])
+        print "%s %s%%" % (stringSimilarity(*test), stringSimilarity(*test, returnPercent=True))
