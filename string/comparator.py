cc88b23d06a138cc3dd25b8b658485d6153d3c87
Michael Tils <mtils@web-utils.de>
2012-01-26 18:03:09 +0000

    Refs #228: Massenverarbeitung auf Datenbank-Änderungen angepasst * Tests der verschiedenen Methoden zum Vergleichen von Strings eingebaut * StringComparator geschrieben, der typische Synonyme berücksichtigt

---
+++ string/comparator.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+'''
+Created on 26.01.2012
+
+@author: michi
+'''
+
+from difflib import SequenceMatcher
+
+synonyms_de_DE = {
+                  u'ß':'ss',
+                  u'ä':'ae',
+                  u'ü':'ue',
+                  u'ö':'oe',
+                  u'Str.':'Strasse',
+                  u'str.':'strasse'
+                  } 
+class StringComparator(object):
+    
+    valForOnlyCaseDiff = 0.99
+    synonymDiffRange = (0.15,0.85)
+    synonymCorrection = 0.05
+    
+    def __init__(self, typicalSynonyms={}):
+        self.typicalSynonyms = typicalSynonyms
+    
+    def compare(self, a, b):
+        if a == b:
+            return 1.0
+        if a.lower() == b.lower():
+            return self.valForOnlyCaseDiff
+        
+        
+        matcher = SequenceMatcher()
+        matcher.set_seqs(a, b)
+        ratio = matcher.ratio()
+        if ratio > self.synonymDiffRange[0] and \
+            ratio < self.synonymDiffRange[1]:
+            translatedA = a
+            translatedB = b
+            for key in self.typicalSynonyms:
+                translatedA = translatedA.replace(key, self.typicalSynonyms[key])
+                translatedB = translatedB.replace(key, self.typicalSynonyms[key])
+            matcher = SequenceMatcher()
+            matcher.set_seqs(translatedA, translatedB)
+            
+            ratio = matcher.ratio() - self.synonymCorrection
+        return ratio
+            
+                
--- util.py
+++ util.py
@@ -4,6 +4,7 @@ Created on 02.07.2011
 
 @author: michi
 '''
+from difflib import SequenceMatcher
 
 class GenClause(object):
     def __init__(self, left):
@@ -66,8 +67,27 @@ class GenClause(object):
     
     def __str__(self):
         return "%s %s %s" % (self.left, self.operator, self.right)
-    
-def stringSimilarity(a,b, returnPercent=False):
+
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s1:
+        return len(s2)
+ 
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+ 
+    return previous_row[-1]
+
+def stringSimilarity(a, b, returnPercent=False):
     "Calculates the Levenshtein distance between a and b."
     aLen = len(a)
     n, m = len(a), len(b)
@@ -87,9 +107,17 @@ def stringSimilarity(a,b, returnPercent=False):
             current[j] = min(add, delete, change)
     if returnPercent:
         #print "Lang: %s" % aLen
-        #rightLetters = float(aLen - current[n])
+        rightLetters = float(aLen - current[n])
+        wrongLetters = current[n]
         #print "Richtige: %s" % rightLetters
-        #print "Richtig: %s" % ( aLen / rightLetters)
+        #print "Falsche: %s" % wrongLetters
+        #print "Richtig: %s" % ( 100.0 - (float(current[n]) / (float(aLen)/100.0)))
+        
+        #matcher = SequenceMatcher()
+        #matcher.set_seqs(a, b)
+        #print "SequenceMatcher: {0}".format(matcher.ratio()*100)
+        #print "Falsche: {0}".format(levenshtein(a, b))
+        
         try:
             result = 100.0 - (float(current[n]) / (float(aLen)/100.0))
         except ZeroDivisionError:
@@ -100,19 +128,22 @@ def stringSimilarity(a,b, returnPercent=False):
     return current[n]
 
 if __name__ == '__main__':
-#    tests = (
-#             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstr."),
-#             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstraße"),
-#             ("Östliche Rheinbrückenstraße","Rheinbrückenstr."),
-#             ("Östliche Rheinbrückenstraße","Östl. Rheinbrückenstr."),
-#             ("Östliche Rheinbrückenstraße","Marienweg"),
-#             ("Hauptstraße","Hauptstr."),
-#             )
-#    for test in tests:
-#        print "%s (%s) %s:" % (test[0], len(test[0]), test[1])
-#        print "%s %s%%" % (stringSimilarity(*test), stringSimilarity(*test, returnPercent=True))
-    clause = GenClause('oma.name').like == 'Pups'
-    print clause
+    tests = (
+             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Östliche Rheinbrückenstraße"),
+             ("Östliche Rheinbrückenstraße","Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Östl. Rheinbrückenstr."),
+             ("Östliche Rheinbrückenstraße","Marienweg"),
+             ("Hauptstraße","Hauptstr."),
+             ("Cresbacher Str.", "Breslauer Str."),
+             (u"Im Gässle", u"Im Gäßle"),
+             (u"Hafenweg", u"HAFENWEG")
+             )
+    for test in tests:
+        print "%s (%s) %s:" % (test[0], len(test[0]), test[1])
+        print "%s %s%%" % (stringSimilarity(*test), stringSimilarity(*test, returnPercent=True))
+#    clause = GenClause('oma.name').like == 'Pups'
+#    print clause
 
 def splitIntAlpha(intString):
     integer = []
@@ -132,5 +163,7 @@ def splitIntAlpha(intString):
             continue
         string.append(unicode(char))
         firstNonDigitFound = True
-  
-    return (int("".join(integer)), "".join(string).strip())
+    try:
+        return (int("".join(integer)), "".join(string).strip())
+    except ValueError:
+        return (0,'')
