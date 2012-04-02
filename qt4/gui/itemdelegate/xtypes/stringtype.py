6cdc351363f7efad53fd1b7b17bc719d4d334235
Michael Tils <mtils@web-utils.de>
2012-04-02 13:33:36 +0000

    Completer CaseInSensitive gemacht

--- qt4/gui/itemdelegate/xtypes/stringtype.py
+++ qt4/gui/itemdelegate/xtypes/stringtype.py
@@ -3,13 +3,23 @@ Created on 04.03.2012
 
 @author: michi
 '''
-from PyQt4.QtCore import QString
-from PyQt4.QtGui import QLineEdit, QCompleter
+from PyQt4.QtCore import QString, Qt
+from PyQt4.QtGui import QLineEdit, QCompleter, QStringListModel, \
+    QAbstractProxyModel
 
 from ems.xtype.base import StringType #@UnresolvedImport
 from ems.qt4.gui.itemdelegate.xtypedelegate import XTypeDelegate #@UnresolvedImport
 
+class HintListModel(QAbstractProxyModel):
+    def __init__(self, parent=None):
+        QAbstractProxyModel.__init__(self, parent)
+    
+    def match(self, start, role, value, hits=1, flags=Qt.MatchStartsWith | Qt.MatchWrap):
+        print start, value
+        return QStringListModel.match(self, start, role, value, hits, flags)
+
 class StringTypeDelegate(XTypeDelegate):
+    
     def createEditor(self, parent, option, index):
         widget = QLineEdit(parent)
         self.configureEditor(widget, self.xType)
@@ -19,8 +29,9 @@ class StringTypeDelegate(XTypeDelegate):
         XTypeDelegate.configureEditor(self, widget, xType)
         widget.setMaxLength(self.xType.maxLength)
         widget.setAlignment(self.textAlignment)
-        if hasattr(self.xType,'hints') and len(self.xType.hints):
+        if hasattr(self.xType, 'hints') and len(self.xType.hints):
             completer = QCompleter(self.xType.hints, self)
+            completer.setCaseSensitivity(Qt.CaseInsensitive)
             widget.setCompleter(completer)
         
         
