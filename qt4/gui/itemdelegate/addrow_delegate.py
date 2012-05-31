036c88186fdf9963b13e6202550f9323cc9200b8
Michael Tils <mtils@web-utils.de>
2012-05-31 20:06:24 +0000

    print raus

--- qt4/gui/itemdelegate/addrow_delegate.py
+++ qt4/gui/itemdelegate/addrow_delegate.py
@@ -24,7 +24,6 @@ class AddRowDelegate(QStyledItemDelegate):
         options.rect = option.rect
         options.state = QStyle.State_Enabled
         if self.addIcon and self.removeIcon:
-            print self.addIcon
             action = variant_to_pyobject(index.data(Qt.UserRole))
             if action == AddRowProxyModel.ADD:
                 options.icon = self.addIcon
