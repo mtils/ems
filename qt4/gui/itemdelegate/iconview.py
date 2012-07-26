0a0832081bbceb0b4c4fd88ffcb305da07f58410
Michael Tils <mtils@web-utils.de>
2012-07-26 19:20:55 +0000

    Fixes #291: Text in Akkordion Navi eingebaut

--- qt4/gui/itemdelegate/iconview.py
+++ qt4/gui/itemdelegate/iconview.py
@@ -4,41 +4,42 @@ Created on 05.07.2011
 @author: michi
 '''
 
-from PyQt4.QtCore import QSize, QRect, QPoint
+from PyQt4.QtCore import QSize, QRect, QPoint, Qt
 from PyQt4.QtGui import QStyledItemDelegate, QStyleOptionViewItemV4, \
     QApplication, QTextDocument, QStyle, QAbstractTextDocumentLayout, QIcon,\
-    QPainter
+    QPainter, QFontMetrics, QPalette
+from ems.qt4.util import variant_to_pyobject
 
 class IconViewDelegate(QStyledItemDelegate):
+    
+    def __init__(self, *args, **kwargs):
+        QStyledItemDelegate.__init__(self, *args, **kwargs)
+        self.drawText = True
+        self.alwaysUseActiveStyle = False
+    
     def paint(self, painter, option, index):
         options = QStyleOptionViewItemV4(option)
         self.initStyleOption(options, index)
         
         style = QApplication.style() if options.widget is None \
             else options.widget.style()
-        self.iconSize = QSize(60,60)
+        
+        if self.alwaysUseActiveStyle:
+            options.state = options.state | QStyle.State_HasFocus | QStyle.State_Active
+        
         painter.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform)
-        options.text = ""
-        myIcon = QIcon(options.icon)
-        options.icon = QIcon()
-        style.drawControl(QStyle.CE_ItemViewItem, options, painter)
-        leftTop = QPoint(options.rect.center().x()-self.iconSize.width()/2,
-                         options.rect.center().y()-self.iconSize.height()/2)
-        noTextRect = QRect( leftTop, self.iconSize)
-        painter.drawImage(noTextRect, myIcon.pixmap(self.iconSize).toImage())
         
+        options.decorationPosition = QStyleOptionViewItemV4.Top
         
-#        textRect = style.subElementRect(QStyle.SE_ItemViewItemText,
-#                                        options)
-        
-        #painter.save()
-        #painter.translate(textRect.topLeft())
-        #painter.setClipRect(textRect.translated(-textRect.topLeft()))
-        #painter.setClipRect(textRect)
-        
-        #doc.documentLayout().draw(painter, ctx)
-        
-        #painter.restore()
+        if not self.drawText:
+            options.text = ""
+            options.displayAlignment = Qt.AlignCenter | Qt.AlignVCenter
+            style.drawControl(QStyle.CE_ItemViewItem, options, painter)
+        else:
+            options.displayAlignment = Qt.AlignCenter
+            style.drawControl(QStyle.CE_ItemViewItem, options, painter)
+
+            
     
     def sizeHint_(self, option, index, manualText=None):
         options = QStyleOptionViewItemV4(option)
