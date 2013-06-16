a85755d40b1f7178f16db364348d649c0e7ed88b
Michael Tils <mtils@web-utils.de>
2013-06-16 21:00:46 +0000

    QStyleEditor angefangen

---
+++ qt4/gui/syntaxhighlighter/csshighlighter.py
@@ -0,0 +1,165 @@
+from PyQt4.QtGui import QSyntaxHighlighter, QTextCharFormat
+from PyQt4.QtCore import Qt
+#http://searchcode.com/codesearch/view/33945919
+class CssHighlighter(QSyntaxHighlighter):
+
+    Selector = 0
+    Property = 1
+    Value = 2
+    Pseudo = 3
+    Pseudo1 = 4
+    Pseudo2 = 5
+    Quote = 6
+    MaybeComment = 7
+    Comment = 8
+    MaybeCommentEnd = 9
+
+    ALNUM = 0
+    LBRACE = 1
+    RBRACE = 2
+    COLON = 3
+    SEMICOLON = 4
+    COMMA = 5
+    QUOTE = 6
+    SLASH = 7
+    STAR = 8
+
+    transitions = (
+        (Selector, Property, Selector, Pseudo,    Property, Selector, Quote, MaybeComment, Selector), #Selector
+        (Property, Property, Selector, Value,     Property, Property, Quote, MaybeComment, Property), #Property
+        (Value,    Property, Selector, Value,     Property, Value,    Quote, MaybeComment, Value), #Value
+        (Pseudo1, Property, Selector, Pseudo2,    Selector, Selector, Quote, MaybeComment, Pseudo), #Pseudo
+        (Pseudo1, Property, Selector, Pseudo,    Selector, Selector, Quote, MaybeComment, Pseudo1), #Pseudo1
+        (Pseudo2, Property, Selector, Pseudo,    Selector, Selector, Quote, MaybeComment, Pseudo2), #Pseudo2
+        (Quote,    Quote,    Quote,    Quote,     Quote,    Quote,   -1, Quote, Quote), #Quote
+        (-1, -1, -1, -1, -1, -1, -1, -1, Comment), #MaybeComment
+        (Comment, Comment, Comment, Comment, Comment, Comment, Comment, Comment, MaybeCommentEnd), #Comment
+        (Comment, Comment, Comment, Comment, Comment, Comment, Comment, -1, MaybeCommentEnd) #MaybeCommentEnd
+    )
+
+    def highlightBlock(self, text):
+        print unicode('highlightBlock "{0}"').format(text)
+        lastIndex = 0
+        lastWasSlash = False
+        state = self.previousBlockState()
+        print "previousBlockState", state
+        saveState = 0
+        #text = unicode(text)
+
+        if state == -1:
+            #As long as the text is empty, leave the state undetermined
+            if text.isEmpty():
+                self.setCurrentBlockState(-1)
+                return
+            '''The initial state is based on the precense of a : and the absense of a {.
+               This is because Qt style sheets support both a full stylesheet as well as
+               an inline form with just properties.'''
+            if text.indexOf(':') > -1 and text.indexOf('{') == -1:
+                state = saveState = CssHighlighter.Property
+            else:
+                state = saveState = CssHighlighter.Selector
+        else:
+            #state = state>>16
+            #saveState = state
+            print "before", state
+
+            saveState = state>>16
+            #state = 255
+            #state = 0x00ff
+            #state = saveState + 0
+            print saveState, state
+
+        if state == CssHighlighter.MaybeCommentEnd:
+            state = CssHighlighter.Comment
+        elif state == CssHighlighter.MaybeComment:
+            state = saveState
+
+        for i in range(text.length()):
+            token = CssHighlighter.ALNUM
+            c = text.at(i)
+            a = unicode(c)
+
+            if state == CssHighlighter.Quote:
+                if a == '\\':
+                    lastWasSlash = True
+                else:
+                    if a == '"' and not lastWasSlash:
+                        token = CssHighlighter.QUOTE
+                    lastWasSlash = False
+            else:
+                if a == '{':
+                    token = CssHighlighter.LBRACE
+                elif a == '}':
+                    token = CssHighlighter.RBRACE
+                elif a == ':':
+                    token = CssHighlighter.COLON
+                elif a == ';':
+                    token = CssHighlighter.SEMICOLON
+                elif a == ',':
+                    token = CssHighlighter.COMMA
+                elif a == '"':
+                    token = CssHighlighter.QUOTE
+                elif a == '/':
+                    token = CssHighlighter.SLASH
+                elif a == '*':
+                    token = CssHighlighter.STAR
+
+            try:
+                newState = CssHighlighter.transitions[state][token]
+            except IndexError:
+                #TODO: Dirty Fix
+                try:
+                    newState = CssHighlighter.transitions[(state>>16)][token]
+                except IndexError:
+                    print "newState is", state, token, (state>>16)
+                    newState = -1
+
+            if newState != state:
+                includeToken = newState == CssHighlighter.MaybeCommentEnd \
+                    or state == CssHighlighter.MaybeCommentEnd and newState != CssHighlighter.Comment \
+                        or state == CssHighlighter.Quote
+                self.highlight(text, lastIndex, i-lastIndex+int(includeToken), state)
+
+                if newState == CssHighlighter.Comment:
+                    lastIndex = i-1 #include the slash and star
+                else:
+                    if token == CssHighlighter.ALNUM or newState == CssHighlighter.Quote:
+                        lastIndex = i
+                    else:
+                        lastIndex = i + 1
+
+            if newState == -1:
+                state = saveState
+            elif state <= CssHighlighter.Pseudo2:
+                saveState = state
+                state = newState
+            else:
+                state = newState
+
+        self.highlight(text, lastIndex, text.length() - lastIndex, state)
+        nextState = state + (saveState<<16)
+        print "nextState", nextState, state
+        #newState = CssHighlighter.transitions[(state>>16)][token]
+        self.setCurrentBlockState(nextState)
+
+    def highlight(self, text, start, length, state):
+        if start >= text.length() or length <= 0:
+            return
+
+        format = QTextCharFormat()
+
+        if state == CssHighlighter.Selector:
+            self.setFormat(start, length, Qt.darkRed)
+        elif state == CssHighlighter.Property:
+            self.setFormat(start, length, Qt.blue)
+        elif state == CssHighlighter.Value:
+            self.setFormat(start, length, Qt.black)
+        elif state == CssHighlighter.Pseudo1:
+            self.setFormat(start, length, Qt.darkRed)
+        elif state == CssHighlighter.Pseudo2:
+            self.setFormat(start, length, Qt.darkRed)
+        elif state == CssHighlighter.Quote:
+            self.setFormat(start, length, Qt.darkMagenta)
+        elif state in (CssHighlighter.Comment, CssHighlighter.MaybeCommentEnd):
+            format.setForeground(Qt.darkGreen)
+            self.setFormat(start, length, format)
---
+++ qt4/gui/widgets/style/qstyleeditor.py
@@ -0,0 +1,244 @@
+from PyQt4.QtGui import QWidget, QHBoxLayout, QGroupBox, QTextEdit, QVBoxLayout,\
+    QFont, QApplication, QStackedWidget, QComboBox, QPushButton, QGridLayout, \
+    QToolButton, QRadioButton, QCheckBox, QCommandLinkButton, QStyleFactory, \
+    QStyle, QTabWidget, QLineEdit, QSpinBox, QDateTimeEdit, QFrame, QFormLayout,\
+    QLabel, QAction, QSlider, QScrollBar,QProgressBar, QDial, QTableWidget, \
+    QListWidget, QTreeWidget, QColumnView, QScrollArea, QTableWidgetItem,\
+    QTreeWidgetItem, QSplitter
+from PyQt4.QtCore import Qt
+
+from ems.qt4.gui.syntaxhighlighter.csshighlighter import CssHighlighter
+
+class QStyleEditor(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self._styles = {}
+        self.refreshAction = QAction('Refresh',self)
+        self.refreshAction.setShortcut(Qt.CTRL + Qt.Key_R)
+        self.refreshAction.triggered.connect(self.applyToPreview)
+        self.setupUi()
+        self.addPreviewPages()
+        self.editor.setText(QApplication.instance().styleSheet())
+        self.setStyleByIndex(0)
+
+    def setupUi(self):
+        self.splitter = QSplitter(self)
+        self.setLayout(QHBoxLayout(self))
+        self.layout().addWidget(self.splitter)
+
+        self.editorGroup = QGroupBox(self)
+        self.editorGroup.setTitle(self.tr('Stylesheet'))
+        self.editorGroup.setLayout(QVBoxLayout(self.editorGroup))
+
+        self.previewGroup = QGroupBox(self)
+        self.previewGroup.setTitle(self.tr('Preview'))
+        self.previewGroup.setLayout(QVBoxLayout(self.previewGroup))
+
+        self.styleSelect = QComboBox(self.previewGroup)
+        for s in QStyleFactory.keys():
+            self.styleSelect.addItem(s)
+        self.previewGroup.layout().addWidget(self.styleSelect)
+        self.styleSelect.currentIndexChanged.connect(self.setStyleByIndex)
+
+        self.previewView = QTabWidget(self.previewGroup)
+        self.previewGroup.layout().addWidget(self.previewView)
+
+        self.editor = QTextEdit(self)
+        self.editorGroup.layout().addWidget(self.editor)
+
+        font = QFont('Monospace')
+        font.setStyleHint(QFont.TypeWriter)
+        font.setPointSize(10)
+        self.editor.setFont(font)
+        
+        self.highlighter = CssHighlighter(self.editor.document())
+        #self.editor
+
+        self.previewButton = QToolButton(self)
+        #self.previewButton.setText(self.tr('Refresh'))
+        #self.previewButton.setShortcut()
+        self.previewButton.setDefaultAction(self.refreshAction)
+        #self.grabShortcut('Ctrl+R')
+        #self.previewButton.clicked.connect(self.applyOnPreview)
+        self.previewGroup.layout().addWidget(self.previewButton)
+
+        self.splitter.addWidget(self.editorGroup)
+        self.splitter.addWidget(self.previewGroup)
+
+    def applyToPreview(self):
+        self.previewView.setStyleSheet(self.editor.document().toPlainText())
+        #self.previewView.style().polish()
+
+    def addPreviewPages(self):
+        self.previewView.addTab(self.formPage(), self.formPage().windowTitle())
+        self.previewView.addTab(self.sliderPage(), self.sliderPage().windowTitle())
+        self.previewView.addTab(self.itemViewPage(), self.itemViewPage().windowTitle())
+
+    def formPage(self):
+        if not hasattr(self, '_formPage'):
+            mainPage = QWidget()
+            mainPage.setWindowTitle(self.tr('Input Widgets'))
+            mainPage.setLayout(QGridLayout(mainPage))
+
+            sampleButton = QPushButton('QPushButton',mainPage)
+            mainPage.layout().addWidget(sampleButton,0, 0)
+
+            sampleToolButton = QToolButton(mainPage)
+            sampleToolButton.setText('QToolButton')
+            mainPage.layout().addWidget(sampleToolButton,0, 1)
+
+            line = QFrame(mainPage)
+            line.setFrameShape(QFrame.HLine)
+            line.setFrameShadow(QFrame.Sunken)
+            mainPage.layout().addWidget(line,1,0,1,2)
+
+            sampleRadioButton = QRadioButton('QRadioButton', mainPage)
+            mainPage.layout().addWidget(sampleRadioButton, 2, 0)
+
+            sampleRadioButton2 = QRadioButton('QRadioButton', mainPage)
+            mainPage.layout().addWidget(sampleRadioButton2, 3, 0)
+
+            checkBox = QCheckBox(mainPage)
+            checkBox.setText(self.tr('QCheckBox'))
+            checkBox.setTristate(True)
+            checkBox.setChecked(True)
+            mainPage.layout().addWidget(checkBox, 2, 1)
+
+            checkBox2 = QCheckBox(mainPage)
+            checkBox2.setText(self.tr('QCheckBox'))
+            checkBox2.setTristate(True)
+            checkBox2.setChecked(False)
+            mainPage.layout().addWidget(checkBox2, 3, 1)
+
+            inputGroup = QGroupBox(mainPage)
+            inputGroup.setTitle(self.tr('QGroupBox'))
+            inputGroup.setLayout(QVBoxLayout(inputGroup))
+
+            combo = QComboBox(inputGroup)
+            combo.addItem(self.tr('QComboBox'))
+            inputGroup.layout().addWidget(combo)
+
+            lineEdit = QLineEdit(inputGroup)
+            lineEdit.setText(self.tr('QLineEdit'))
+            inputGroup.layout().addWidget(lineEdit)
+
+            textEdit = QTextEdit(inputGroup)
+            textEdit.setText(self.tr('QTextEdit'))
+            inputGroup.layout().addWidget(textEdit)
+
+            spinboxFrame = QFrame(inputGroup)
+            spinboxFrame.setFrameStyle(spinboxFrame.StyledPanel)
+            spinboxFrame.setLayout(QFormLayout(spinboxFrame))
+
+            spinBox = QSpinBox(spinboxFrame)
+            #spinBox.setSpecialValueText(self.tr('QSpinBox'))
+            spinboxFrame.layout().addRow(QLabel(self.tr('QSpinBox:')), spinBox)
+
+            dateTimeEdit = QDateTimeEdit(spinboxFrame)
+            spinboxFrame.layout().addRow(QLabel(self.tr('QDateTimeEdit:')), dateTimeEdit)
+            inputGroup.layout().addWidget(spinboxFrame)
+
+            mainPage.layout().addWidget(inputGroup,4,0,1,2)
+            self._formPage = mainPage
+
+        return self._formPage
+    
+    def sliderPage(self):
+        if not hasattr(self,'_sliderPage'):
+            sliderPage = QWidget()
+            sliderPage.setWindowTitle(self.tr('Slider'))
+            sliderPage.setLayout(QGridLayout(sliderPage))
+            
+            hSlider = QSlider(Qt.Horizontal, sliderPage)
+            sliderPage.layout().addWidget(QLabel(self.tr('QSlider')),0,0)
+            sliderPage.layout().addWidget(hSlider,0,1)
+            
+            hScrollbar = QScrollBar(Qt.Horizontal, sliderPage)
+            sliderPage.layout().addWidget(QLabel(self.tr('QScrollBar')),1,0)
+            sliderPage.layout().addWidget(hScrollbar,1,1)
+            
+            progressBar = QProgressBar(sliderPage)
+            progressBar.setValue(50)
+            sliderPage.layout().addWidget(QLabel(self.tr('QProgressBar')),2,0)
+            sliderPage.layout().addWidget(progressBar,2,1)
+            
+            dial = QDial(sliderPage)
+            dial.setValue(50)
+            dial.setMaximumHeight(150)
+            sliderPage.layout().addWidget(QLabel(self.tr('QDial')),3,0)
+            sliderPage.layout().addWidget(dial,3,1)
+            self._sliderPage = sliderPage
+        return self._sliderPage
+    
+    def itemViewPage(self):
+        if not hasattr(self,'_itemViewPage'):
+            itemViewPage = QWidget()
+            itemViewPage.setWindowTitle(self.tr('Item-Views'))
+            itemViewPage.setLayout(QVBoxLayout(itemViewPage))
+
+            listWidget = QListWidget(itemViewPage)
+            listWidget.addItem('QListWidget 1')
+            listWidget.addItem('QListWidget 2')
+            itemViewPage.layout().addWidget(listWidget)
+
+            tableWidget = QTableWidget(itemViewPage)
+            tableWidget.setColumnCount(3)
+            tableWidget.setRowCount(3)
+            tableWidget.setHorizontalHeaderLabels(('QTableWidget 1','QTableWidget 2','QTableWidget 3'))
+            for i in range(4):
+                for j in range(4):
+                    item = QTableWidgetItem('Cell ' + str(i) + '-' + str(j))
+                    tableWidget.setItem(i,j,item)
+            tableWidget.horizontalHeader().setResizeMode(tableWidget.horizontalHeader().Stretch)
+            itemViewPage.layout().addWidget(tableWidget)
+
+            treeWidget = QTreeWidget(itemViewPage)
+            treeWidget.setHeaderLabels(('QTreeWidget 1','QTreeWidget 2'))
+            treeWidget.setColumnCount(2)
+            items = []
+            for i in range(3):
+                item = QTreeWidgetItem(['col 1','col 2'])
+
+                for j in range(2):
+                    childItem = QTreeWidgetItem(item, ['child col 1','child col 2'])
+                items.append(item)
+
+            treeWidget.addTopLevelItems(items)
+            itemViewPage.layout().addWidget(treeWidget)
+            
+            
+            
+            self._itemViewPage = itemViewPage
+        return self._itemViewPage
+    
+    def specialsPage(self):
+        raise NotImplementedError()
+        '''QCommandLinkButton
+           QToolBox
+           QGraphicsView
+           QCalendar
+           QLCDNumber
+           QSplitter
+           QMenu
+           QToolBar'''
+
+    def setStyleByIndex(self, index):
+        styleName = str(self.styleSelect.itemText(index))
+        if not self._styles.has_key(styleName):
+            self._styles[styleName] = QStyleFactory.create(styleName)
+        
+        self.previewView.setStyle(self._styles[styleName])
+        for widget in self.previewView.findChildren(QWidget):
+            widget.setStyle(self._styles[styleName])
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    
+    editor = QStyleEditor()
+    editor.setMinimumWidth(800)
+    editor.setMinimumHeight(600)
+    editor.show()
+    
+    sys,exit(app.exec_())
