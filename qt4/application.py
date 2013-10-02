2c6024d5a96e92abaa99c2906bf3787a9e6b3a0a
Michael Tils <mtils@web-utils.de>
2013-10-02 13:35:07 +0000

    * Changed include to lib.*

--- qt4/application.py
+++ qt4/application.py
@@ -8,7 +8,7 @@ import os.path
 from PyQt4.QtCore import QTimer
 from PyQt4.QtGui import QApplication, QAction
 
-from ems.util import platformName
+from lib.ems.util import platformName
 
 class MainApplication(QApplication):
     '''
