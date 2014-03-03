6753e4a747b6c9c5277b04c13af493f755451041
Michael Tils <mtils@web-utils.de>
2014-03-03 20:40:15 +0000

    * Added Possibility to overwrite certain widgets

--- qt4/gui/mapper/sa/delegate/itemview.py
+++ qt4/gui/mapper/sa/delegate/itemview.py
@@ -16,24 +16,36 @@ class MapperItemViewDelegate(GenericDelegate):
         self.model = model
         self.mapper = mapper
         self._columnDelegates = {}
+        self._overwrittenDelegates = {}
+        self._columnNameCache = {}
         self.model.modelReset.connect(self.resetDelegates)
     
     
     def _getDelegate(self, index):
         col = index.column()
-        if not self._columnDelegates.has_key(col):
-            columnName = variant_to_pyobject(index.data(qt4.ColumnNameRole))
+
+        if col not in self._columnDelegates:
+
+            if col not in self._columnNameCache:
+                self._columnNameCache[col] = variant_to_pyobject(index.data(qt4.ColumnNameRole))
+
+            columnName = self._columnNameCache[col]
+
+            if columnName in self._overwrittenDelegates:
+                return self._overwrittenDelegates[columnName]
+
             self._columnDelegates[col] = self.mapper.getDelegateForItem(columnName)
-#            strategy = self.mapper.getStrategyFor(columnName)
-#            self._columnDelegates[col] = strategy.getDelegate(self.ormObj, columnName)
-#            if self._columnDelegates[col]:
-#                print "Custom Delegate for col {0}".format(columnName)
+
         return self._columnDelegates[col]
     
     def resetDelegates(self):
         #print "Resetting delegates..."
         self._columnDelegates.clear()
-    
+        self._columnNameCache.clear()
+
+    def overWriteColumnDelegate(self, colName, delegate):
+        self._overwrittenDelegates[colName] = delegate
+
     '''These two methods have no use inside this functionality until yet '''
     def insertColumnDelegate(self, column, delegate):
         raise TypeError("The mapper chooses a delegate," +
