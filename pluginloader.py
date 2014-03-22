c1ce4b8ae19813e50594681ad05f2baa78fd8d1f
Michael Tils <mtils@web-utils.de>
2014-03-22 15:52:45 +0000

    * Changed API for plugin adding. use PluginLoader += plugin * Added an event if plugins are added

--- pluginloader.py
+++ pluginloader.py
@@ -5,22 +5,31 @@ Created on 20.10.2013
 @author: michi
 '''
 
+from eventhook import EventHook
+
 class Plugin(object):
     def match(self, query):
         raise NotImplementedError()
 
 class PluginLoader(object):
 
-    plugins = []
+    _plugins = []
+
+    pluginAdded = EventHook()
+
+    def __iadd__(self, plugin):
+        self._plugins.append(plugin)
+        PluginLoader.pluginAdded.fire(plugin)
+        return self
 
     def _find_plugin(self, query):
-        for plugin in self.plugins:
+        for plugin in self._plugins:
             if plugin.match(query):
                 return plugin
 
     def _find_plugins(self, query):
         plugins = []
-        for plugin in self.plugins:
+        for plugin in self._plugins:
             if plugin.match(query):
                 plugins.append(plugin)
         return plugins
