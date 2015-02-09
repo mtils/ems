a113fa24e958fc5d857dd743c1b3175571fc2236
Michael Tils <mtils@web-utils.de>
2015-02-09 19:24:46 +0100

    Added pypi stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'ems',
+  packages = ['ems'],
+  version = '0.1',
+  description = 'Small collection of classes to build model centric applications with python and Qt',
+  author = 'Michael Tils',
+  author_email = 'mtils@web-utils.de',
+  url = 'https://github.com/mtils/ems',
+  download_url = 'https://github.com/mtils/ems/tarball/0.1',
+  keywords = ['qt', 'gui', 'rad','scaffolding'],
+  classifiers = [],
+)
