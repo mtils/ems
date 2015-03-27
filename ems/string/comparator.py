72d296ae3cd1da7c733626200e27cf3e8aa2bbc9
Michael Tils <mtils@web-utils.de>
2015-03-27 15:55:13 +0100

    Fixed wrong synonym order

--- ems/string/comparator.py
+++ ems/string/comparator.py
@@ -4,19 +4,18 @@ Created on 26.01.2012
 
 @author: michi
 '''
-
+from collections import OrderedDict
 from difflib import SequenceMatcher
 
-synonyms_de_DE = {
-                  u'ß':'ss',
-                  u'ä':'ae',
-                  u'ü':'ue',
-                  u'ö':'oe',
-                  u'Str.':'Strasse',
-                  u'str.':'strasse',
-                  u'Str.':u'Straße',
-                  u'str.':u'Straße',
-                  } 
+synonyms_de_DE = OrderedDict((
+                  (u'ß','ss'),
+                  (u'ä','ae'),
+                  (u'ü','ue'),
+                  (u'ö','oe'),
+                  (u' Str.',' Strasse'),
+                  (u'-Str.','-Strasse'),
+                  (u'str.','strasse')
+                  ))
 class StringComparator(object):
 
     valForOnlyCaseDiff = 0.99
@@ -47,6 +46,7 @@ class StringComparator(object):
             for key in self.typicalSynonyms:
                 translatedA = translatedA.replace(key, self.typicalSynonyms[key])
                 translatedB = translatedB.replace(key, self.typicalSynonyms[key])
+
             matcher = SequenceMatcher()
             matcher.set_seqs(translatedA.lower(), translatedB.lower())
 
@@ -60,7 +60,6 @@ class StringComparator(object):
 
         for candidate in pool:
             percent = self.compare(candidate, reference)*100.0
-
             if percent >= min_percent:
                 pool_by_similarity[percent] = candidate
 
