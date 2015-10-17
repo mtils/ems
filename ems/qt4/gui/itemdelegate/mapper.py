1cf1e1f5e66c95fce6c719c355218d5b146726b1
Michael Tils <mtils@web-utils.de>
2015-10-17 12:20:45 +0200

    Added new mapper delegates for both orientations

--- ems/qt4/gui/itemdelegate/mapper.py
+++ ems/qt4/gui/itemdelegate/mapper.py
@@ -5,7 +5,7 @@ Created on 20.03.2012
 '''
 
 from ems import qt4
-from ems.qt4.util import variant_to_pyobject
+from ems.qt4.util import variant_to_pyobject as py
 from ems.qt4.gui.itemdelegate.genericdelegate import GenericDelegate
 from ems.qt4.itemmodel.reflectable_mixin import ReflectableMixin #@UnresolvedImport
 
@@ -31,16 +31,15 @@ class MapperItemViewDelegate(GenericDelegate):
         return self._mapper
     
     mapper = property(getMapper)
-    
+
     def _getDelegate(self, index):
         col = index.column()
         if col not in self._columnDelegates:
-            model = index.model()
-            if isinstance(model, ReflectableMixin):
-                type_ = model.columnType(index.column())
-            self._columnDelegates[col] = self._mapper.getDelegateForItem(type_)
+            xtype = py(index.data(qt4.XTypeRole))
+            self._columnDelegates[col] = self._mapper.getDelegateForItem(xtype)
+
         return self._columnDelegates[col]
-    
+
     def resetDelegates(self):
         #print "Resetting delegates..."
         self._columnDelegates.clear()
@@ -54,3 +53,37 @@ class MapperItemViewDelegate(GenericDelegate):
         raise TypeError("The mapper controls the delegates," + 
                         "use GenericDelegate instead")
 
+class XTypeViewDelegate(GenericDelegate):
+    def __init__(self, mapper, parent=None):
+        super(XTypeViewDelegate, self).__init__(parent)
+        self._mapper = mapper
+        self._typeDelegates = {}
+
+    def count(self):
+        return len(self._typeDelegates)
+
+    def getMapper(self):
+        return self._mapper
+
+    mapper = property(getMapper)
+
+    def _getDelegate(self, index):
+
+        xtype = py(index.data(qt4.XTypeRole))
+        cacheId = id(xtype)
+
+        if cacheId not in self._typeDelegates:
+            self._typeDelegates[cacheId] = self._mapper.getDelegateForItem(xtype)
+
+        return self._typeDelegates[cacheId]
+
+    def resetDelegates(self):
+        self._typeDelegates.clear()
+
+    '''These two methods have no use inside this functionality until yet '''
+    def insertColumnDelegate(self, column, delegate):
+        raise TypeError("The mapper chooses a delegate," +
+                        " use GenericDelegate instead")
+    def removeColumnDelegate(self, column):
+        raise TypeError("The mapper controls the delegates," + 
+                        "use GenericDelegate instead")
