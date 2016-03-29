d635084c1a9a3980be3f0eee2e5e41d6beb743f0
Michael Tils <mtils@web-utils.de>
2016-03-29 22:03:16 +0200

    Refactored editor in facade class

---
+++ ems/qt/graphics/graphics_widget.py
@@ -0,0 +1,101 @@
+
+from ems.qt import QtWidgets, QtGui, QtCore
+from ems.qt.graphics.tool import GraphicsToolDispatcher
+from ems.qt.graphics.text_tool import TextTool
+from ems.qt.edit_actions import EditActions
+from ems.qt.richtext.char_format_actions import CharFormatActions
+from ems.qt.richtext.block_format_actions import BlockFormatActions
+from ems.qt.layout.toolbararea import ToolBarArea
+from ems.qt.graphics.graphics_view import GraphicsView
+from ems.qt.graphics.graphics_scene import GraphicsScene
+from ems.qt.tool_widgets.one_of_a_list_slider import OneOfAListSlider
+from ems.qt.graphics.page_item import PageItem
+
+Qt = QtCore.Qt
+QWidget = QtWidgets.QWidget
+QVBoxLayout = QtWidgets.QVBoxLayout
+QToolBar = QtWidgets.QToolBar
+QSlider = QtWidgets.QSlider
+
+class GraphicsWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super(GraphicsWidget, self).__init__(parent)
+        self._zoomSteps = (50,75,100,150,200,300,500)
+        self._pageSize = (595, 842) # A4 in points
+        self._setUpUi()
+        self._setUpTools()
+        self._addToolsToToolbars()
+        self._connectTools()
+        self._setupPageFormat()
+
+    def _setUpUi(self):
+        self.setLayout(QVBoxLayout())
+        self.toolBars = ToolBarArea(self)
+        self.addToolBar = QToolBar()
+        self.textToolbar = QToolBar()
+        self.layout().addWidget(self.toolBars)
+        self.view = GraphicsView()
+        self.layout().addWidget(self.view)
+        self._zoomSlider = self._createZoomSlider(self, self._zoomSteps)
+        self._zoomSlider.listValueChanged.connect(self.view.setZoom)
+        self.layout().addWidget(self._zoomSlider)
+        self.scene = GraphicsScene()
+        self.view.setScene(self.scene)
+        self.scene.setSceneRect(0, 0, self._pageSize[0], self._pageSize[1])
+
+
+
+    def _setUpTools(self):
+        self.tools = GraphicsToolDispatcher(self)
+        self.tools.setScene(self.scene)
+        self.textTool = TextTool()
+        self.tools.addTool(self.textTool)
+        self.editActions = EditActions(self)
+        self.charFormatActions = CharFormatActions(self)
+        self.blockFormatActions = BlockFormatActions(self)
+
+        self.scene.focusItemChanged.connect(self.tools.updateFocusItem)
+        self.view.emptyAreaClicked.connect(self.tools.addItemAt)
+
+
+    def _addToolsToToolbars(self):
+        self.editActions.addToToolbar(self.addToolBar)
+        self.addToolBar.addSeparator()
+        for action in self.tools.actions:
+            self.addToolBar.addAction(action)
+        self.charFormatActions.addToToolbar(self.textToolbar, addActions=False)
+        self.charFormatActions.addToToolbar(self.textToolbar, addWidgets=False)
+        self.blockFormatActions.addToToolbar(self.textToolbar, addWidgets=False)
+        self.toolBars.addToolBar(self.addToolBar)
+        self.toolBars.addToolBarBreak()
+        self.toolBars.addToolBar(self.textToolbar)
+
+    def _createZoomSlider(self, parent, zoomSteps):
+        zoomSlider = OneOfAListSlider(parent)
+        zoomSlider.setValueList(zoomSteps)
+        zoomSlider.setListValue(100)
+        zoomSlider.setOrientation(Qt.Horizontal)
+        zoomSlider.setTickInterval(1)
+        zoomSlider.setTickPosition(QSlider.TicksBothSides)
+        return zoomSlider
+
+    def _connectTools(self):
+        self.textTool.currentCharFormatChanged.connect(self.charFormatActions.signals.updateCharFormatWithoutDiffs)
+        self.textTool.currentBlockFormatChanged.connect(self.blockFormatActions.signals.setBlockFormat)
+        self.textTool.undoAvailable.connect(self.editActions.actionUndo.setEnabled)
+        self.textTool.redoAvailable.connect(self.editActions.actionRedo.setEnabled)
+        self.textTool.copyAvailable.connect(self.editActions.actionCopy.setEnabled)
+        self.textTool.copyAvailable.connect(self.editActions.actionCut.setEnabled)
+        self.editActions.actionUndo.triggered.connect(self.textTool.undo)
+        self.editActions.actionRedo.triggered.connect(self.textTool.redo)
+        self.editActions.actionCopy.triggered.connect(self.textTool.copy)
+        self.editActions.actionCut.triggered.connect(self.textTool.cut)
+        self.editActions.actionPaste.triggered.connect(self.textTool.paste)
+
+        self.charFormatActions.signals.charFormatDiffChanged.connect(self.textTool.mergeFormatOnWordOrSelection)
+        self.blockFormatActions.signals.blockFormatModified.connect(self.textTool.setBlockFormatOnCurrentBlock)
+
+    def _setupPageFormat(self):
+        page = PageItem()
+        self.scene.addItem(page)
--- examples/qt/test_richtext_item.py
+++ examples/qt/test_richtext_item.py
@@ -1,117 +1,13 @@
 
-from __future__ import print_function
-
 import os
 
-from ems.qt import QtWidgets, QtGui, QtCore
+from ems.qt import QtCore
+from ems.qt.graphics.graphics_widget import GraphicsWidget
 
-from ems.qt.richtext.inline_edit_graphicsitem import TextItem
-from ems.qt.richtext.char_format_actions import CharFormatActions
-from ems.qt.richtext.block_format_actions import BlockFormatActions
-from ems.qt.layout.toolbararea import ToolBarArea
-from ems.qt.graphics.tool import GraphicsToolDispatcher
-from ems.qt.graphics.text_tool import TextTool
-from ems.qt.graphics.selection_rect import SelectionRect
-from ems.qt.graphics.page_item import PageItem
-from ems.qt.tool_widgets.one_of_a_list_slider import OneOfAListSlider
-from ems.qt.edit_actions import EditActions
-from ems.qt.graphics.graphics_view import GraphicsView
-from ems.qt.graphics.graphics_scene import GraphicsScene
-
-QAction = QtWidgets.QAction
-pyqtSignal = QtCore.pyqtSignal
-Qt = QtCore.Qt
-QGraphicsView = QtWidgets.QGraphicsView
-QGraphicsScene = QtWidgets.QGraphicsScene
-QWidget = QtWidgets.QWidget
-QVBoxLayout = QtWidgets.QVBoxLayout
-QToolBar = QtWidgets.QToolBar
 QResource = QtCore.QResource
-QPainter = QtGui.QPainter
-QColor = QtGui.QColor
-QSlider = QtWidgets.QSlider
-QPointF = QtCore.QPointF
-QTransform = QtGui.QTransform
 
 resourcePath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','..','ems','qt4','gui','widgets','icons.rcc'))
 QResource.registerResource(resourcePath)
 
-PageSize = (595, 842) # A4 in points
-
-dialog = QWidget()
-
-dialog.tools = GraphicsToolDispatcher(dialog)
-
-textTool = TextTool()
-dialog.tools.addTool(textTool)
-
-dialog.editActions = EditActions(dialog)
-dialog.charFormatActions = CharFormatActions(dialog)
-dialog.blockFormatActions = BlockFormatActions(dialog)
-
-dialog.setLayout(QVBoxLayout())
-dialog.toolBars = ToolBarArea(dialog)
-dialog.addToolBar = QToolBar()
-dialog.textToolbar = QToolBar()
-#dialog.textWidgetToolbar = QToolBar()
-
-#dialog.toolbarArea = QToolBar(dialog)
-dialog.layout().addWidget(dialog.toolBars)
-
-dialog.editActions.addToToolbar(dialog.addToolBar)
-dialog.addToolBar.addSeparator()
-
-for action in dialog.tools.actions:
-    dialog.addToolBar.addAction(action)
-
-dialog.charFormatActions.addToToolbar(dialog.textToolbar, addActions=False)
-dialog.charFormatActions.addToToolbar(dialog.textToolbar, addWidgets=False)
-dialog.blockFormatActions.addToToolbar(dialog.textToolbar, addWidgets=False)
-
-dialog.toolBars.addToolBar(dialog.addToolBar)
-dialog.toolBars.addToolBarBreak()
-dialog.toolBars.addToolBar(dialog.textToolbar)
-#dialog.toolBars.addToolBarBreak()
-#dialog.toolBars.addToolBar(dialog.textWidgetToolbar)
-
-
-zoomSteps = (50,75,100,150,200,300,500)
-
-view = GraphicsView()
-dialog.layout().addWidget(view)
-dialog.zoomSlider = OneOfAListSlider(dialog)
-dialog.zoomSlider.setValueList(zoomSteps)
-dialog.zoomSlider.setListValue(100)
-dialog.zoomSlider.setOrientation(Qt.Horizontal)
-dialog.zoomSlider.setTickInterval(1)
-dialog.zoomSlider.setTickPosition(QSlider.TicksBothSides)
-dialog.zoomSlider.listValueChanged.connect(view.setZoom)
-dialog.layout().addWidget(dialog.zoomSlider)
-scene = GraphicsScene()
-scene.setSceneRect(0, 0, PageSize[0], PageSize[1])
-scene.focusItemChanged.connect(dialog.tools.updateFocusItem)
-view.setScene(scene)
-dialog.tools.setScene(scene)
-
-view.emptyAreaClicked.connect(dialog.tools.addItemAt)
-
-page = PageItem()
-scene.addItem(page)
-
-#textItem = TextItem('Hallo', QPointF(15.0,15.0), view.scene() )
-
-textTool.currentCharFormatChanged.connect(dialog.charFormatActions.signals.updateCharFormatWithoutDiffs)
-textTool.currentBlockFormatChanged.connect(dialog.blockFormatActions.signals.setBlockFormat)
-textTool.undoAvailable.connect(dialog.editActions.actionUndo.setEnabled)
-textTool.redoAvailable.connect(dialog.editActions.actionRedo.setEnabled)
-textTool.copyAvailable.connect(dialog.editActions.actionCopy.setEnabled)
-textTool.copyAvailable.connect(dialog.editActions.actionCut.setEnabled)
-dialog.editActions.actionUndo.triggered.connect(textTool.undo)
-dialog.editActions.actionRedo.triggered.connect(textTool.redo)
-dialog.editActions.actionCopy.triggered.connect(textTool.copy)
-dialog.editActions.actionCut.triggered.connect(textTool.cut)
-dialog.editActions.actionPaste.triggered.connect(textTool.paste)
-
-dialog.charFormatActions.signals.charFormatDiffChanged.connect(textTool.mergeFormatOnWordOrSelection)
-dialog.blockFormatActions.signals.blockFormatModified.connect(textTool.setBlockFormatOnCurrentBlock)
+dialog = GraphicsWidget()
 dialog.show()
