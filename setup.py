a6fd75cc8a109405280a3149fa0e2635accb6d6e
Michael Tils <mtils@web-utils.de>
2016-03-31 11:36:32 +0200

    Raised pypi version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
   name = 'ems',
   packages = find_packages(),
-  version = '0.2.16',
+  version = '0.2.17',
   description = 'Small collection of classes to build model centric applications with python and Qt',
   author = 'Michael Tils',
   author_email = 'mtils@web-utils.de',
   url = 'https://github.com/mtils/ems',
-  download_url = 'https://github.com/mtils/ems/tarball/0.2.16',
+  download_url = 'https://github.com/mtils/ems/tarball/0.2.17',
   keywords = ['qt', 'gui', 'rad','scaffolding'],
   install_requires = [
     # 'dbfpy', not installable via pip
